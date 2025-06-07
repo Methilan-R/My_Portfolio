6ccd8da109dd1c55bec98a29f7b8a8239149df60
Methilan-R <rmethilan@gmail.com>
2025-06-07 10:53:49 +0530

    image

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/hexa.png"
+        src="/images/hexa.png"
         alt="Methilan Rameswaran"
         className="max-h-[450px] w-auto"
       />
